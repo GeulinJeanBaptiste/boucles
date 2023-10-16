3ca1d456986cfe6519ebc191c0c462ae2ab427e7
jean-baptiste Geulin <jeanbaptiste.geulin@gmail.com>
2023-10-16 14:42:44 +0200

    Boucles

---
+++ assets/script.js
@@ -0,0 +1,17 @@
+const tableau = ["entree1", "entree2", "entree3"];
+// boucle
+// foreach
+tableau.forEach((value, index) => {
+  console.log(value, index);
+});
+// for
+for (let i = 0; i < tableau.length; i++) {
+  // const element = tableau[i];
+  console.log(i);
+}
+// while
+let n = 0;
+while (n < 10) {
+  console.log(n);
+  n++;
+}
